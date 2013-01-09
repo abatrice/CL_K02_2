828c7db38941f11f28c00cc774e88a344f8ec33d
onohara kenta <ababababa123456@gmail.com>
2013-01-09 10:32:21 +0900

    応用プログラミングA課題１

---
+++ CL_K02_2/CL_K02_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* 作成者：1T1(11) 小野原健太 */
+/* 課題名：課題02_2 */
+void main(void) {
+	int a,b;
+	printf("aの値を入力してください : ");
+	scanf("%d",&a);
+	printf("bの値を入力してください : ");
+	scanf("%d",&b);
+	if (b != 0){
+		printf("%dを%dで割った結果は%dです\n",a,b,a/b);
+		printf("%dを%dで割った余りは%dです\n",a,b,a%b);
+	} else {
+		printf("エラー : ０で割ることはできません");
+	}
+}
